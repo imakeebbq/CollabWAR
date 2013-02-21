d55123108ccc00022316e9986a42d0fdabbfbfcd
imakeebbq <legend_one41@hotmail.com>
2013-02-21 19:17:52 +1100

    Update BlackJack/BlackJack/Form1.vb

--- BlackJack/BlackJack/Form1.vb
+++ BlackJack/BlackJack/Form1.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Form1
-
+Dim A
+    Dim K
+    Dim Q
+    Dim J
+Private Sub Card5Table2_Click(sender As Object, e As EventArgs) Handles Card5Table2.Click
+If A = True Then A = 10
+        If K = True Then K = 10
+        If Q = True Then Q = 10
+        If J = True Then J = 10
+        If Val(21) = 21 Then MsgBox("You have blackjack!")
+        If Val(22>) = 22> Then MsgBox("you have went over better luck next time")
+        If Val(<21) = <21 then msgbox("stay?")
 End Class
