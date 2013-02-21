edbf77f254cee9ee2887d39891298b71361eb281
imakeebbq <legend_one41@hotmail.com>
2013-02-21 19:18:41 +1100

    Update BlackJack/BlackJack/Form1.vb

--- BlackJack/BlackJack/Form1.vb
+++ BlackJack/BlackJack/Form1.vb
@@ -1,14 +1,87 @@
-﻿Public Class Form1
-Dim A
-    Dim K
-    Dim Q
-    Dim J
-Private Sub Card5Table2_Click(sender As Object, e As EventArgs) Handles Card5Table2.Click
-If A = True Then A = 10
+Public Class Form1
+    Dim A
+    Dim K
+    Dim Q
+    Dim J
+
+    Private Sub Card1_Click(sender As Object, e As EventArgs) Handles Card1.Click
+        If A = True Then A = 10
+        If K = True Then K = 10
+        If Q = True Then Q = 10
+        If J = True Then J = 10
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Card2_Click(sender As Object, e As EventArgs) Handles Card2.Click
+        If A = True Then A = 10
+        If K = True Then K = 10
+        If Q = True Then Q = 10
+        If J = True Then J = 10
+    End Sub
+
+    Private Sub Card3_Click(sender As Object, e As EventArgs) Handles Card3.Click
+        If A = True Then A = 10
+        If K = True Then K = 10
+        If Q = True Then Q = 10
+        If J = True Then J = 10
+    End Sub
+
+    Private Sub Card4_Click(sender As Object, e As EventArgs) Handles Card4.Click
+        If A = True Then A = 10
+        If K = True Then K = 10
+        If Q = True Then Q = 10
+        If J = True Then J = 10
+    End Sub
+
+    Private Sub Card5_Click(sender As Object, e As EventArgs) Handles Card5.Click
+        If A = True Then A = 10
+        If K = True Then K = 10
+        If Q = True Then Q = 10
+        If J = True Then J = 10
+    End Sub
+
+    Private Sub Card1Table2_Click(sender As Object, e As EventArgs) Handles Card1Table2.Click
+        If A = True Then A = 10
+        If K = True Then K = 10
+        If Q = True Then Q = 10
+        If J = True Then J = 10
+    End Sub
+
+    Private Sub Card2Table2_Click(sender As Object, e As EventArgs) Handles Card2Table2.Click
+        If A = True Then A = 10
+        If K = True Then K = 10
+        If Q = True Then Q = 10
+        If J = True Then J = 10
+    End Sub
+
+    Private Sub Card3Table2_Click(sender As Object, e As EventArgs) Handles Card3Table2.Click
+        If A = True Then A = 10
+        If K = True Then K = 10
+        If Q = True Then Q = 10
+        If J = True Then J = 10
+    End Sub
+
+    Private Sub Card4Table2_Click(sender As Object, e As EventArgs) Handles Card4Table2.Click
+        If A = True Then A = 10
+        If K = True Then K = 10
+        If Q = True Then Q = 10
+        If J = True Then J = 10
+    End Sub
+
+    Private Sub Card5Table2_Click(sender As Object, e As EventArgs) Handles Card5Table2.Click
+        If A = True Then A = 10
         If K = True Then K = 10
         If Q = True Then Q = 10
         If J = True Then J = 10
         If Val(21) = 21 Then MsgBox("You have blackjack!")
         If Val(22>) = 22> Then MsgBox("you have went over better luck next time")
         If Val(<21) = <21 then msgbox("stay?")
+
+        End If
+    End Sub
 End Class
